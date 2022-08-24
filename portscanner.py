7d90b4b4725595c435af6c0fbdd6a9b5e0ce8054
Adamv7 <107083704+Adamv7@users.noreply.github.com>
2022-08-23 22:12:23 -0700

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,32 @@
+#From Udemy Course 
+# Complete Ethical Hacking Bootcamp 2022: Zero to Mastery by Andrei
+
+
+import socket
+import termcolor
+
+
+def scan(target, ports):
+	print('\n' + ' Starting Scan For ' + str(target))
+	for port in range(1,ports):
+		scan_port(target,port)
+
+
+def scan_port(ipaddress, port):
+	try:
+		sock = socket.socket()
+		sock.connect((ipaddress, port))
+		print("[+] Port Opened " + str(port))
+		sock.close()
+	except:
+		pass
+
+
+targets = input("[*] Enter Targets To Scan(split them by ,): ")
+ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
+if ',' in targets:
+	print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
+	for ip_addr in targets.split(','):
+		scan(ip_addr.strip(' '), ports)
+else:
+	scan(targets,ports)
